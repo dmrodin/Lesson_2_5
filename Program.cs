b4bd80abd5f958efa186f577f2a7028cebd5a25b
Dmitry Rodin <dmrodin@bk.ru>
2022-03-02 11:48:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_2_5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            bool result = value == (int)value;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
